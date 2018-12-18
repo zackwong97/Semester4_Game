d7952a224b511affa114c39bb0a9d23cebbdad04
zackwong97 <42438597+zackwong97@users.noreply.github.com>
2018-12-18 18:33:57 +0800

    Change Project settings & disable some plugins

--- Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
+++ Plugins/AdvancedSessions/AdvancedSteamSessions/Intermediate/Build/Win64/UE4/Development/AdvancedSteamSessions/Definitions.AdvancedSteamSessions.h
@@ -37,15 +37,15 @@
 #define PLATFORM_WINDOWS 1
 #define DEPTH_32_BIT_CONVERSION 0
 #define PROJECT_COPYRIGHT_STRING Fill out your copyright notice in the Description page of Project Settings.
-#define PROJECT_PRODUCT_NAME Virtual Reality BP Game Template
-#define PROJECT_PRODUCT_IDENTIFIER VRExpPluginExample
+#define PROJECT_PRODUCT_NAME UE4Game
+#define PROJECT_PRODUCT_IDENTIFIER Semester4_Game
 #define NDEBUG 1
 #define UE_BUILD_DEVELOPMENT 1
-#define UE_ENGINE_DIRECTORY "../../../../../../../Program Files/Epic Games/UE_4.20/Engine/"
-#define ORIGINAL_FILE_NAME "VRExpPluginExample.exe"
+#define UE_ENGINE_DIRECTORY "../../../../../../Program Files/Epic Games/UE_4.20/Engine/"
+#define ORIGINAL_FILE_NAME "Semester4_Game.exe"
 #define UE_IS_ENGINE_MODULE 0
 #define UE_BUILD_DEVELOPMENT_WITH_DEBUGGAME 0
-#define UE_PROJECT_NAME VRExpPluginExample
+#define UE_PROJECT_NAME Semester4_Game
 #define WITH_ADVANCED_STEAM_SESSIONS 1
 #define IMPLEMENT_ENCRYPTION_KEY_REGISTRATION() 
 #define IMPLEMENT_SIGNING_KEY_REGISTRATION() 
